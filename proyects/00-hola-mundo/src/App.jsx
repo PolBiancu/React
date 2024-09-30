100eb10aea1aacbc944d48a8eca670a9d54125a5
PolBiancu <biancullipablo@gmail.com>
2024-09-30 11:34:13 -0300

    P3

--- proyects/00-hola-mundo/src/App.jsx
+++ proyects/00-hola-mundo/src/App.jsx
@@ -7,7 +7,7 @@ export function App () {
         <section className='App'>
             <TwitterFollowCard isFollowing = {true} userName = "leomessi" name = "Pablo Bianculli"/>
             <TwitterFollowCard isFollowing = {false} userName = "pablo_biancu" name = "Pablo Bianculli"/>
-            <TwitterFollowCard userName = "pablo_biancu" name = "Pablo Bianculli"/>
+            <TwitterFollowCard isFollowing ={true} userName = "pablo_biancu" name = "Pablo Bianculli"/>
         </section>
     )
 }
