aa45c81e724683cd4abaf102e108768986dafb33
PolBiancu <biancullipablo@gmail.com>
2024-10-01 00:05:16 -0300

    P1

--- proyects/02-ta-te-ti/src/App.jsx
+++ proyects/02-ta-te-ti/src/App.jsx
@@ -1,8 +1,40 @@
-import './App.css'
+import { Children } from "react"
+
+const TURNS = {
+  X: 'x',
+  O: 'o'
+}
+
+const board = Array(9).fill(null)
+
+const Square = ({ children, updateBoar, index}) => {
+  return (
+      <dir className = "square">
+        {children}
+      </dir>
+  )
+
+}
 
 function App() {
   return (
-    <h1>Ta te ti</h1>
+    <main className="board">
+      <h1>TA TE TI</h1>
+      <section className="game">
+        {
+          board.map((_, index)=> {
+            return(
+              <Square
+                key = {index}
+                index = {index}
+              >
+                {index}
+              </Square>
+            )
+          })
+        }
+      </section>
+    </main>
   )
 }
 
